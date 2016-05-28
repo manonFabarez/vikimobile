8ba4e9bda422e97c95951c22d5bfe786fd3e6b80
Pauline <tastetpauline@gmail.com>
2016-05-28 16:08:14 +0200

    Pauline - new pass

--- app/src/main/java/com/viki/vikiapplication/BackgroundTask.java
+++ app/src/main/java/com/viki/vikiapplication/BackgroundTask.java
@@ -28,6 +28,9 @@ import static android.support.v4.app.ActivityCompat.startActivity;
 public class BackgroundTask extends AsyncTask <String, Void, String>{
 
     //Variables
+    final String EXTRAT_IDP = "idP";
+    static String idP;
+    String values[] = new String[2];
     Context ctx;
     AlertDialog alertDialog;
     BackgroundTask(Context ctx){
@@ -42,6 +45,7 @@ public class BackgroundTask extends AsyncTask <String, Void, String>{
         //url de la page php à exécuter
         String url_connect = "http://virtual-kine.ddns.net/mobile/login.php";
         String url_mdpo = "http://virtual-kine.ddns.net/mobile/mdpo.php";
+        String url_newMdp = "http://virtual-kine.ddns.net/mobile/newMdp.php";
 
         //En fonction du nom de la méthode passée en paramètre -->Action
          switch (method) {
@@ -132,7 +136,57 @@ public class BackgroundTask extends AsyncTask <String, Void, String>{
                  } catch (IOException e) {
                      e.printStackTrace();
                  }
+
+             case "newMdp":
+
+                 //Récupération paramètres
+                 String old_mdp = params[1];
+                 String new_mdp = params[2];
+                 String conf_new_mdp = params[3];
+
+                 //Création de l'url d'accès a la page php + paramétrage + stockage dans le buffer
+                 try {
+                     URL url = new URL(url_newMdp);
+                     HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                     httpURLConnection.setRequestMethod("POST");
+                     httpURLConnection.setDoOutput(true);
+                     httpURLConnection.setDoInput(true);
+                     OutputStream outputStream = httpURLConnection.getOutputStream();
+                     BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                     String data = URLEncoder.encode("old_mdp", "UTF-8") + "=" + URLEncoder.encode(old_mdp, "UTF-8") + "&" +
+                                   URLEncoder.encode("new_mdp", "UTF-8") + "=" + URLEncoder.encode(new_mdp, "UTF-8") + "&" +
+                                   URLEncoder.encode("conf_new_mdp", "UTF-8") + "=" + URLEncoder.encode(conf_new_mdp, "UTF-8") + "&" +
+                             URLEncoder.encode("idP", "UTF-8") + "=" + URLEncoder.encode(idP, "UTF-8");
+                     bufferedWriter.write(data);
+                     bufferedWriter.flush();
+                     bufferedWriter.close();
+                     outputStream.close();
+                     InputStream inputStream = httpURLConnection.getInputStream();
+                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                     String response = "";
+                     String line = "";
+
+                     //Lecture du resultat retourné
+                     while ((line = bufferedReader.readLine()) != null) {
+                         response += line;
+                     }
+
+                     //Fermeture des objets
+                     bufferedReader.close();
+                     inputStream.close();
+                     httpURLConnection.disconnect();
+
+                     //retour de la réponse
+                     return response;
+
+                 } catch (MalformedURLException e) {
+                     e.printStackTrace();
+                 } catch (IOException e) {
+                     e.printStackTrace();
+                 }
+                 break;
              }
+
             return null;
     }
 
@@ -150,9 +204,13 @@ public class BackgroundTask extends AsyncTask <String, Void, String>{
     @Override
     protected void onPostExecute(String result) {
 
-        switch (result) {
+        values = result.split("-");
+        switch (values[0]) {
             case "connexionOK": //Connexion application réussie
-                ctx.startActivity(new Intent(ctx, Menu.class));
+                idP = values[1];
+                Intent i = new Intent(ctx, Menu.class);
+                i.putExtra("id",idP);
+                ctx.startActivity(i);
                 break;
             case "connexionKO": //Connexion application echec
                 alertDialog.setMessage("Identifiant et/ou mot de passe incorrect. Veuillez recommencer");
@@ -168,7 +226,15 @@ public class BackgroundTask extends AsyncTask <String, Void, String>{
                 alertDialog.show();
                 break;
             case "newMdpOK": //première connexion avec un nouveau mot de passe
-                ctx.startActivity(new Intent(ctx, ChangerMDP.class));
+                idP = values[1];
+                Intent j = new Intent(ctx, ChangerMDP.class);
+                j.putExtra(EXTRAT_IDP,idP);
+                ctx.startActivity(j);
+                break;
+            case "newMdpKO": // nouveau mot de passe echec
+                alertDialog.setMessage("Une erreur est survenue. Le mot de passe n'a pas été modifié");
+                alertDialog.show();
+                ctx.startActivity(new Intent(ctx,Menu.class));
                 break;
         }
     }
--- app/src/main/java/com/viki/vikiapplication/ChangerMDP.java
+++ app/src/main/java/com/viki/vikiapplication/ChangerMDP.java
@@ -1,13 +1,45 @@
 package com.viki.vikiapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class ChangerMDP extends AppCompatActivity {
+
+    //Variables
+    EditText ET_OLD, ET_NEW, ET_CONF;
+    String old_mdp, new_mdp, conf_new_mdp;
+    String idP;
+    final String EXTRAT_IDP = "idP";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_changer_mdp);
+
+        //Récupération de l'id du patient
+        Intent i = getIntent();
+        if(i != null){
+            idP = i.getStringExtra(EXTRAT_IDP);
+        }
     }
+
+    public void valider_chgerMdp_OnClick(View V) {
+
+        //Récupération de l'info dans un string
+        old_mdp = ET_OLD.getText().toString();
+        new_mdp = ET_NEW.getText().toString();
+        conf_new_mdp = ET_CONF.getText().toString();
+
+        //Passement des infos dans une tâche en arrière plan : class BackGroundTask
+        String method = "newMdp";
+        BackgroundTask backgroundTask = new BackgroundTask(this);
+
+        //Exécution tache en arrière plan + paramètres necessaires à la taches
+        backgroundTask.execute(method,old_mdp,new_mdp,conf_new_mdp,idP);
+
+    }
+
 }
